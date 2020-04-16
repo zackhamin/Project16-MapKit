8c7d74eafe36d15cea28975bfbd2804005ff4913
Zack <ishaqamincoop@icloud.com>
2020-04-16 12:39:24 +0100

    mapview and delegate added

---
+++ Project16-MapKit/Capital.swift
@@ -0,0 +1,13 @@
+//
+//  Capital.swift
+//  Project16-MapKit
+//
+//  Created by Ishaq Amin on 16/04/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class Capital: NSObject {
+
+}
